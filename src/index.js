01d5d2575e1794e468db546b7c65be67a8c4aa59
William Nogueira <will.ribeiro@live.com>
2021-12-15 23:29:22 -0300

    criando saque na conta

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const customers = [];
  * statement - [] array
  */
 
-// Middleware
+// Middlewares
 function verifyIfExistsAccountCPF(request, response, next) {
   const { cpf } = request.headers; // request.headers ou request.params passando "/statement/:cpf"
 
@@ -27,6 +27,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
   request.customer = customer;
 
   return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
 }
 
 app.post("/account", (request, response) => {
@@ -69,6 +81,27 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
   customer.statement.push(statementOperation)
 
   return response.status(201).send();
+})
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request; // pegar informações de quanto
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!"})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send()
 })
 
 app.listen(3333);
